1a59e101796359cf9246d4573a107aa96236761e
David Sanchez <davidesj97@gmail.com>
2021-08-25 14:11:23 -0500

    Terminé el HTML para poder calcular el precio con descuento

--- Curso-practico-JavaScript/descuentos/descuentos.js
+++ Curso-practico-JavaScript/descuentos/descuentos.js
@@ -3,3 +3,13 @@ function calcularPrecioConDescuento(precio, descuento) {
     const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
     return precioConDescuento;
 }
+
+function generarDescuento() {
+    const precio = document.getElementById("precioProducto").value;
+    const descuento = document.getElementById("descuentoProducto").value;
+
+    const precioConDescuento = calcularPrecioConDescuento(precio, descuento);
+
+    const resultadoP = document.getElementById("resultado");
+    resultadoP.innerText = "El precio con descuento son: $" + precioConDescuento;
+}
