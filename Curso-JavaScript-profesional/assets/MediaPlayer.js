39a17129ab8455689774a0bb005c2fb67ca901d2
David Sanchez <davidesj97@gmail.com>
2021-09-03 11:48:30 -0500

    Creé un plugin para que el video se reproduzca automaticamente y tambien creé y configure un boton para silenciar o activar el audio del video

--- Curso-JavaScript-profesional/assets/MediaPlayer.js
+++ Curso-JavaScript-profesional/assets/MediaPlayer.js
@@ -1,21 +1,38 @@
-class MediaPlayer {
-    constructor(config) {
-        this.media = config.el;
-    }
-    play() {
-        this.media.play();
-    }
-    pause() {
-        this.media.pause();
-    }
-    togglePlay() {
-        if (this.media.paused) {
-            this.play();
-        } else {
-            this.pause();
-        }
-    }
+function MediaPlayer(config) {
+    this.media = config.el;
+    this.plugins = config.plugins || [];
+
+    this._initPlugins();
 }
+
+MediaPlayer.prototype._initPlugins = function() {
+    this.plugins.forEach(plugin => {
+        plugin.run(this);
+    });
+}
+
+MediaPlayer.prototype.play = function() {
+    this.media.play();
+};
+MediaPlayer.prototype.pause = function() {
+    this.media.pause();
+};
+MediaPlayer.prototype.togglePlay = function() {
+    if (this.media.paused) {
+        this.play();
+    } else {
+        this.pause();
+    }
+};
+MediaPlayer.prototype.mute = function() {
+    this.media.muted = true;
+};
+MediaPlayer.prototype.unmute = function() {
+    this.media.muted = false;
+}
+MediaPlayer.prototype.toggleMute = function() {
+    this.media.muted = !this.media.muted;
+};
 
 
 
