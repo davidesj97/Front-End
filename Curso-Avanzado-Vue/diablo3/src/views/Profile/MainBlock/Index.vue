9f2d63fc3908234ece1a8a8bc6f09ed8d9550b20
David Sánchez <davidesj97@gmail.com>
2021-10-12 13:12:51 -0500

    Creación del bloque que muestra los stats y el tiempo jugado de los heroes

--- Curso-Avanzado-Vue/diablo3/src/views/Profile/MainBlock/Index.vue
+++ Curso-Avanzado-Vue/diablo3/src/views/Profile/MainBlock/Index.vue
@@ -3,9 +3,10 @@
     <div class="grid-item item-left">
       <top-heroes v-if="hasHeroes" :heroes="topHeroes"/>
       <heroes-list v-if="hasHeroesList" :heroes="heroesList" />
+      <progress-list :acts="profileData.progression"/>
     </div>
     <div class="grid-item item-right">
-      <h1>Derecha</h1>
+      <player-stats :stats="statsData"/>
     </div>
   </div>
 </template>
@@ -13,12 +14,16 @@
 <script>
 import TopHeroes from './TopHeroes/Index'
 import HeroesList from './HeroesList/Index.vue'
+import ProgressList from './ProgressList/Index'
+import PlayerStats from './PlayerStats/Index'
 
 export default {
   name: 'MainBlock',
   components: {
     TopHeroes,
-    HeroesList
+    HeroesList,
+    ProgressList,
+    PlayerStats
   },
   props: {
     profileData: {
@@ -41,6 +46,10 @@ export default {
     },
     heroesList() {
       return this.profileData.heroes.slice(3, this.profileData.heroes.length)
+    },
+    statsData() {
+      const { paragonLevel, kills, timePlayed } = this.profileData
+      return { paragonLevel, kills, timePlayed }
     }
   }
 }
