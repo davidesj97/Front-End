3aaf727b4ac0ce54f3ebec4dba2b5dfa079f7e52
David Sánchez <davidesj97@gmail.com>
2021-10-13 09:14:22 -0500

    Creación de la vista de heroe con sus atributos y sus habilidades activas y pasivas

--- Curso-Avanzado-Vue/diablo3/src/api/search.js
+++ Curso-Avanzado-Vue/diablo3/src/api/search.js
@@ -10,14 +10,14 @@ import { locales } from '../utils/regions'
 const protocol = 'https://'
 const host = '.api.blizzard.com/'
 
-/*
- * Returns the specified account profile.
- * GET – /d3/profile/{account}
- * Los parámetros que hemos obtenido a través de la URL
- *  - @param region {String}
- *  - @param account {String}
- * @returns {Promise}
- */
+/**
+* Returns the specified account profile.
+* GET – /d3/profile/{account}
+* Los parámetros que hemos obtenido a través de la URL
+*  - @param region {String}
+*  - @param account {String}
+* @returns {Promise}
+*/
 
 function getApiAccount({ region, account }) {
   // Recuerso de la API al que queremos acceder
@@ -40,6 +40,52 @@ function getApiAccount({ region, account }) {
   return get(API_URL, { params })
 }
 
+/**
+* Returns a single hero
+* GET – /d3/profile/{account}/hero/{heroId}
+* @param region {String}
+* @param account {String}
+* @param heroId {String}
+* @returns {Promise}
+*/
+
+function getApiHero({ region, account, heroId }) {
+  const resource = `d3/profile/${account}/hero/${heroId}`
+  const API_URL = `${protocol}${region}${host}${resource}`
+  const locale = locales[region]
+
+  const params = {
+    access_token: store.state.oauth.accessToken,
+    locale
+  }
+
+  return get(API_URL, { params })
+}
+
+/**
+* Returns a list of items for the specified hero.
+* GET – /d3/profile/{account}/hero/{heroId}/items
+* @param region {String}
+* @param account {String}
+* @param heroId {String}
+* @returns {Promise}
+*/
+
+function getApiDetailedHeroItems({ region, account, heroId }) {
+  const resource = `d3/profile/${account}/hero/${heroId}/items`
+  const API_URL = `${protocol}${region}${host}${resource}`
+  const locale = locales[region]
+
+  const params = {
+    access_token: store.state.oauth.accessToken,
+    locale
+  }
+
+  return get(API_URL, { params })
+}
+
 export {
-  getApiAccount
+  getApiAccount,
+  getApiHero,
+  getApiDetailedHeroItems
 }
